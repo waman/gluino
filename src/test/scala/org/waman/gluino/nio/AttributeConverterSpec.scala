1726f69a1b8b9cb1e5b8f49a00bcc848ade6fa4c
waman <waman@coast.ocn.ne.jp>
2015-10-31 21:26:17 +0900

    Add POSIX file permission conversion tests.

--- src/test/scala/org/waman/gluino/nio/AttributeConverterSpec.scala
+++ src/test/scala/org/waman/gluino/nio/AttributeConverterSpec.scala
@@ -1,14 +1,22 @@
 package org.waman.gluino.nio
 
 import java.nio.file.attribute.FileTime
+import java.nio.file.attribute.PosixFilePermission._
 import java.time.{Instant, OffsetDateTime, ZoneId, ZonedDateTime}
 
 import org.waman.gluino.GluinoCustomSpec
 
 class AttributeConverterSpec extends GluinoCustomSpec with AttributeConverter{
 
-  "***** Utilities for file attributes *****" - {
-
+  "***** POSIX File Permission *****" - {
+    "convertStringToPosixFilePermissionSet() method should" - {
+      "convert String to FileAttribute whose value is Set of PosixFilePermissions" in {
+        __Exercise__
+        val sut = convertStringToPosixFilePermissionSet("rwx------")
+        __Verify__
+        sut.value() should contain only(OWNER_READ, OWNER_WRITE, OWNER_EXECUTE)
+      }
+    }
   }
 
   "***** FileTime conversions *****" - {
